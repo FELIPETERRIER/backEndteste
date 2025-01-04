d1031707f9b402af880be86cafb54fe49ad407ce
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2025-01-03 22:12:43 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+
+//const dotenv = require('dotenv');
+//dotenv.config();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.listen(port,()=>{
+    console.log("servidor rodando na porta " +port)
+})
